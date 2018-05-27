080a29a4c85c3f94eed69539b48057688245ec7f
ufownl <ufownl@gmail.com>
2018-05-27 22:57:05 +0800

    Apply gradient clip

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import mxnet as mx
 from dataset import load_conversations, dataset_filter, make_vocab, tokenize, rnn_batches
 from seq2seq_lstm import Seq2seqLSTM
 
-def main(num_embed, num_hidden, num_layers, batch_size, sequence_length, context):
+def main(num_embed, num_hidden, num_layers, gradient_clip, batch_size, sequence_length, context):
     print("Loading dataset...", flush=True)
     dataset = dataset_filter(load_conversations("data/xiaohuangji50w_nofenci.conv"), sequence_length)
     vocab = make_vocab(dataset)
@@ -42,6 +42,8 @@ def main(num_embed, num_hidden, num_layers, batch_size, sequence_length, context
                 output, hidden = model(source, target, hidden)
                 L = loss(output, label)
                 L.backward()
+            grads = [p.grad(context) for p in model.collect_params().values()]
+            mx.gluon.utils.clip_global_norm(grads, gradient_clip)
             trainer.step(batch_size)
             batch_L = mx.nd.mean(L).asscalar()
             if batch_L != batch_L:
@@ -73,7 +75,7 @@ def main(num_embed, num_hidden, num_layers, batch_size, sequence_length, context
 if __name__ == "__main__":
     while True:
         try:
-            main(num_embed=128, num_hidden=1024, num_layers=2, batch_size=1024, sequence_length=16, context=mx.gpu())
+            main(num_embed=128, num_hidden=1024, num_layers=2, gradient_clip=5, batch_size=1024, sequence_length=16, context=mx.gpu())
             break;
         except ValueError:
             print("Oops! The value of loss become NaN...")
