9d1546a94a3cc4942077c8f04f10a2ed272d6168
RangerUFO <ufownl@gmail.com>
2018-05-28 10:45:11 +0800

    Clip the gradient automatically

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import mxnet as mx
 from dataset import load_conversations, dataset_filter, make_vocab, tokenize, rnn_batches
 from seq2seq_lstm import Seq2seqLSTM
 
-def main(num_embed, num_hidden, num_layers, gradient_clip, batch_size, sequence_length, context):
+def main(num_embed, num_hidden, num_layers, clip_gradient, batch_size, sequence_length, context):
     print("Loading dataset...", flush=True)
     dataset = dataset_filter(load_conversations("data/xiaohuangji50w_nofenci.conv"), sequence_length)
     vocab = make_vocab(dataset)
@@ -31,7 +31,8 @@ def main(num_embed, num_hidden, num_layers, gradient_clip, batch_size, sequence_
         model.initialize(mx.init.Xavier(), ctx=context)
 
     print("Training...", flush=True)
-    trainer = mx.gluon.Trainer(model.collect_params(), "adam", {"learning_rate": learning_rate})
+    trainer = mx.gluon.Trainer(model.collect_params(), "adam",
+                               {"learning_rate": learning_rate, "clip_gradient": clip_gradient})
     while learning_rate >= 1e-5:
         random.shuffle(dataset)
         ts = time.time()
@@ -42,8 +43,6 @@ def main(num_embed, num_hidden, num_layers, gradient_clip, batch_size, sequence_
                 output, hidden = model(source, target, hidden)
                 L = loss(output, label)
                 L.backward()
-            grads = [p.grad(context) for p in model.collect_params().values()]
-            mx.gluon.utils.clip_global_norm(grads, gradient_clip)
             trainer.step(batch_size)
             batch_L = mx.nd.mean(L).asscalar()
             if batch_L != batch_L:
@@ -75,7 +74,7 @@ def main(num_embed, num_hidden, num_layers, gradient_clip, batch_size, sequence_
 if __name__ == "__main__":
     while True:
         try:
-            main(num_embed=128, num_hidden=1024, num_layers=2, gradient_clip=5, batch_size=1024, sequence_length=16, context=mx.gpu())
+            main(num_embed=128, num_hidden=1024, num_layers=2, clip_gradient=5, batch_size=1024, sequence_length=16, context=mx.gpu())
             break;
         except ValueError:
             print("Oops! The value of loss become NaN...")
